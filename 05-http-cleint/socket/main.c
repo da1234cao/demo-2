c8370015d2a66e36da5cbc8fde7ce7880dfb9bca
tongzz <tongzz@koal.com>
2023-05-08 16:42:19 +0800

    adding-sock

---
+++ 05-http-cleint/socket/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include "utils.h"
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+  connection con = {};
+  strncpy(con.url, "https://www.baidu.com", sizeof(con.url));
+  http_parser_parse_url(con.url, strlen(con.url), 0, &con.url_parts);
+  print_connection(&con);
+  return 0;
+}
---
+++ 05-http-cleint/socket/main.h
@@ -0,0 +1,26 @@
+#include "3rd/http_parser.h"
+#include <openssl/ssl.h>
+
+#define URL_MAX_LENGTH 1024
+#define SENDBUF 1024
+#define RECVBUF 8192
+
+typedef enum { OK, ERROR, RETRY } status;
+
+typedef struct connection {
+  http_parser parser;
+  int fd;
+  SSL *ssl;
+  char url[URL_MAX_LENGTH];
+  struct http_parser_url url_parts;
+  char send_uf[SENDBUF];
+  char rec_buf[RECVBUF];
+} connection;
+
+struct sock {
+  status (*connect)(connection *c);
+  status (*read)(connection *c);
+  status (*write)(connection *c);
+  status (*close)(connection *c);
+  status (*readable)(connection *c);
+};
